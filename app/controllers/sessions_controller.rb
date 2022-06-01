a4d1eff401a5b43e1bff3e4dff91436a346cb323
tori-goodell <tori.goodell@kellogg.northwestern.edu>
2022-06-01 16:32:00 +0000

    Set up login and sign up pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Logged in as: #{@user["username"]}"
+        redirect_to "/places"
+      else
+        flash["notice"] = "Incorrect email or password"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Incorrect email or password"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+    def show
+        @user = User.find_by({ "id" => params["id"] })
+      end
+    
+      def new
+        @user = User.new
+      end
+    
+      def create
+        @user = User.new
+        @user["username"] = params["user"]["username"]
+        @user["email"] = params["user"]["email"]
+        @user["password"] = params["user"]["password"]
+        @user.save
+        redirect_to "/users/#{@user["id"]}"
+      end
 end
