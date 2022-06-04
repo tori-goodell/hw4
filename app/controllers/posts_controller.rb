b67b1c229277de30313bd620f25005187384ab70
tori-goodell <tori.goodell@kellogg.northwestern.edu>
2022-06-04 02:39:43 +0000

    added image upload option

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new
     @post["title"] = params["post"]["title"]
+    @post.uploaded_image.attach(params["post"]["uploaded_image"])
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
